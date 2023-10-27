fb7451ad1f7d9413f075941aec28580913ab4984
Yagizcan <yagizpancak@gmail.com>
2023-10-27 15:14:58 +0300

    Initial Commit

---
+++ src/main/java/com/trendyol/bootcamp/TrendyolBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.trendyol.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendyolBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendyolBootcampApplication.class, args);
+	}
+
+}
